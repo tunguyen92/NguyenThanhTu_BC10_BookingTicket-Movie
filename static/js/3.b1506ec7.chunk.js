68e0bd465f4fb69581e67d5f55743e1bf92446b7
tunguyen92 <tunguyen3692@gmail.com>
2021-10-09 08:34:48 +0700

    Updates

--- static/js/3.b1506ec7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmovie=this.webpackJsonpmovie||[]).push([[3],{427:function(e,t,n){"use strict";n.r(t);var c=n(11),o=n(72),r=n(0),a=n(37),j=n(28),s=n(1),b=["Component"];t.default=function(e){var t=e.Component,n=Object(o.a)(e,b);return Object(r.useEffect)((function(){window.scrollTo(0,0)})),localStorage.getItem(j.d)?Object(s.jsx)(a.b,Object(c.a)(Object(c.a)({},n),{},{render:function(e){return Object(s.jsx)(r.Fragment,{children:Object(s.jsx)(t,Object(c.a)({},e))})}})):Object(s.jsx)(a.a,{to:"/login"})}}}]);
-//# sourceMappingURL=3.b1506ec7.chunk.js.map
